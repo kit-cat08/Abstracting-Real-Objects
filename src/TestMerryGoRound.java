0d952558294b9fd5522ece965f6f8a475060e9a5
kit-cat08 <cathyvo2118@gmail.com>
2025-11-13 10:43:48 -0800

    program

--- src/TestMerryGoRound.java
+++ src/TestMerryGoRound.java
@@ -7,9 +7,13 @@
  */
 public class TestMerryGoRound {
     public static void main(String[] args) throws Exception {
+        // Create MerryGoRound objects and test methods
+        // Create Capacity object
         Capacity capacity = new Capacity(100);
+        // Create MerryGoRound objects
         MerryGoRound merryGoRound = new MerryGoRound(20, 20, 5, 10, false, capacity);
         MerryGoRound merryGoRound2 = new MerryGoRound(10, 10, 7);
+        // Test toString method and other functionalities
         System.out.println(merryGoRound.toString());
         System.out.println(merryGoRound2.toString());
         merryGoRound.cleaning();
